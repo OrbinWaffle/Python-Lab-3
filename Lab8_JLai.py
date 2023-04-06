96f5f9d48c2422c8b413f65dec23f6a39c8fb004
OrbinWaffle <79740022+OrbinWaffle@users.noreply.github.com>
2023-04-06 14:33:57 -0700

    added comments and formatting changes

--- Lab8_JLai.py
+++ Lab8_JLai.py
@@ -16,10 +16,12 @@ def TaskOne():
 
     # Read file into lines list
     lines = file.readlines()
-    print(lines[0])
-    print(lines[2])
+    print(lines[0].rstrip("\n"))
+    print(lines[2].rstrip("\n"))
     # Close file
     file.close()
+
+    print()
 
     # Open file
     file = open(dir_path + "\\we_cant_touch.txt", "r")
@@ -28,8 +30,15 @@ def TaskOne():
     # Iterate through lines in file, appending them into the list
     for line in file:
         line_list.append(line)
-    print(line_list[1])
-    print(line_list[3])
+    print(line_list[1].rstrip("\n"))
+    print(line_list[3].rstrip("\n"))
+
+    # Which task would you like to run? Type 1, 2, or 3: 1
+    # We canâ€™t touch
+    # We hunker down
+
+    # But we still reach out
+    # But we still rise up
 
 # Task Two
 
@@ -60,31 +69,41 @@ def TaskTwo():
     # BOB:	So long.
 
 def TaskThree():
-    # Ask whether or not to use relative path
+    # Loop till we get valid input
     while True:
+        # Ask whether or not to use relative path
         user_input = input("Is the file in the current directory? y/n, or q to quit: ")
         file_path = ""
         if(user_input == "y"):
             file_name = input("Please input the file name: ")
+            # Add current directory to file path
             file_path = os.path.dirname(os.path.realpath(__file__)) + "\\" + file_name
         elif(user_input == "q"):
             exit()
         else:
             file_path = input("Please input the direct path to the file: ")
+        # Attempt to open file
         try:
             file = open(file_path, "r")
             break
+        # If file open fails, print error and try again
         except (PermissionError, FileNotFoundError):
             print("Error: Could not find file at path {0} Please try again.\n".format(file_path))
     float_list = []
     for line in file:
+        # For each line, try to convert to float
         try:
             float_val = float(line)
+        # If conversion fails, set to 0.0
         except ValueError:
             float_val = 0.0
         float_list.append(float_val)
     print(float_list)
         
+    # Which task would you like to run? Type 1, 2, or 3: 3
+    # Is the file in the current directory? y/n, or q to quit: y
+    # Please input the file name: numbers.txt
+    # [102.0, 99999.0, 20.5, 90.0, 0.0, 0.0, 2370.3887, 0.0, 0.1, 0.0, 30.2, 0.0, 0.2384234]
 
 # Task selector
 if __name__ == "__main__":
