87635ccf8bfc34ffe03454ac6982102142743f1e
OrbinWaffle <79740022+OrbinWaffle@users.noreply.github.com>
2023-04-25 14:36:07 -0700

    comments

--- Lab10_JLai.py
+++ Lab10_JLai.py
@@ -1,21 +1,23 @@
 # Name: Joshua Lai
 # Lab 10
-# Completed 4/24/2022
+# Completed 4/25/2022
 
-# Base class
+# Java bicycle class
 class Bicycle:
     gear : int
     speed : int
     
+    # Constructor
     def __init__(self, new_gear : int, new_speed : int) -> None:
         self.gear = new_gear
         self.speed = new_speed
     
+    # Functions
     def apply_brake(self, decrement : int):
         self.speed += decrement
     
     def speed_up(self, increment : int):
-        speed += increment
+        self.speed += increment
     
     def to_string(self) -> str:
         return ("No of gears are " + str(self.gear) + "\n" + "speed of bicycle is " + str(self.speed))
@@ -24,6 +26,7 @@ class Bicycle:
 class MountainBike(Bicycle):
     seat_height : int
 
+    # Constructor uses parent constructor
     def __init__(self, new_gear: int, new_speed: int, start_height: int) -> None:
         super().__init__(new_gear, new_speed)
         self.seat_height = start_height
@@ -31,26 +34,31 @@ class MountainBike(Bicycle):
     def set_height(self, new_height : int):
         self.seat_height = new_height
     
+    # to_string uses parent to_string and concatenates an extra part
     def to_string(self) -> str:
         return super().to_string() + ("\nseat height is " + str(self.seat_height))
 
+# Fraction class
 class Fraction:
-    num : float
-    den : float
+    _num : float
+    _den : float
 
+    # Constructor
     def __init__(self, new_num : float, new_den : float) -> None:
-        self.num = new_num
-        self.den = new_den
+        self._num = new_num
+        self._den = new_den
 
+    # Overrides
     def __add__(self, other):
-        return Fraction(self.num * other.den + self.den * other.num, self.den * other.den)
+        return Fraction(self._num * other.den + self._den * other.num, self._den * other.den)
     def __mul__(self, other):
-        return Fraction(self.num * other.num, self.den * other.den)
+        return Fraction(self._num * other.num, self._den * other.den)
     def __eq__(self, other):
-        return (self.num * other.den == self.den * other.num)
+        return (self._num * other.den == self._den * other.num)
     
+    # Overriding string function instead of using a display function
     def __str__(self):
-        return str(self.num) +  "/" + str(self.den)
+        return str(self._num) +  "/" + str(self._den)
 
 
 def main_one():
@@ -80,3 +88,14 @@ if __name__ == "__main__":
         main_one()
     elif (selection == 2):
         main_two()
+
+# OUTPUT:
+# Which task would you like to run? Type 1 or 2: 1
+# No of gears are 3      
+# speed of bicycle is 100
+# seat height is 25
+
+# Which task would you like to run? Type 1 or 2: 2
+# 59/105
+# f1 and f3 not equal
+# f3 and f4 are the same
